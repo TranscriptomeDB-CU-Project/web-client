1c0cf849212fb877087dabf7c87e3eac97a8cdc8
Pimrak Apiratchatanont <apimrak@gmail.com>
2024-02-18 20:33:44 +0700

    feat: add styled components

---
+++ src/app/test/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import styled from 'styled-components'
+
+const AcceptButton = styled.button`
+  background-color: green;
+`
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <AcceptButton>Accept</AcceptButton>
+    </div>
+  )
+}
+
+export default Test
