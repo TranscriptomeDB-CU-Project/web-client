7fdfe53747d2727db84041b3eccbcafec716c60f
miello <35169079+miello@users.noreply.github.com>
2024-04-07 17:44:17 +0700

    feat: [TRA-71] mobile size display and refactor sample section (#25)
    * feat: hide website when in mobile view
    
    * refactor: use table instead of grid
    
    * feat: wrap header with thead

--- src/app/select/components/SampleSection/components/Header/styled.ts
+++ src/app/select/components/SampleSection/components/Header/styled.ts
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const HeaderContainer = styled.th`
+  margin: auto;
   padding: 8px;
-  gap: 4px;
 `
 
 export const TitleContainer = styled.div`
   display: flex;
+  width: fit-content;
+  margin: auto;
+  margin-bottom: 4px;
   align-items: center;
   gap: 4px;
   cursor: pointer;
--- src/app/select/components/SampleSection/components/Sample/styled.ts
+++ src/app/select/components/SampleSection/components/Sample/styled.ts
@@ -1,11 +1,30 @@
 import styled from 'styled-components'
 
 import Text from '@/components/Text'
-import { PALETTE } from '@/theme'
+import { PALETTE, WHITE } from '@/theme'
 
-export const Cell = styled(Text).attrs({ variant: 'body1' })`
-  border: 1px solid ${PALETTE.BLACK[50]};
+export const TextCell = styled(Text).attrs({ variant: 'body1' })`
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+export const TableCell = styled.td`
+  padding: 2px;
+  border: 1px solid ${PALETTE.BLACK[50]};
+`
+
+export const TableCellCheckbox = styled(TableCell)`
+  position: sticky;
+  left: 0;
+  padding-left: 8px;
+  padding-right: 8px;
+  background-color: ${WHITE};
+`
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `
